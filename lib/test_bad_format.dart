74a1a959551773055e5a9f9b890878fe62adeb01
matheus barboza <matheus_jesse@hotmail.com>
2025-09-14 14:03:09 -0300

    test: adiciona arquivo com formatação ruim para testar CI

---
+++ lib/test_bad_format.dart
@@ -0,0 +1,14 @@
+// Arquivo com formatação propositalmente ruim
+class TesteBadFormat{
+String name;int age;
+TesteBadFormat(this.name,this.age){
+print("Bad formatting here");
+}
+void doSomething(){
+if(name.isNotEmpty){
+print("Name: $name");
+}else{
+print("No name");
+}
+}
+}
